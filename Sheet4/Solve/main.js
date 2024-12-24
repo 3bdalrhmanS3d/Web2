31707884081ff0a9755aca55ffb9da44b3905c9f
3bdalrhmanS3d <abdalrhmansaad24@gmail.com>
2024-12-25 01:52:14 +0200

    Add initial implementation for Sheet 4 with XML data loading and person details display

---
+++ Sheet4/Solve/main.js
@@ -0,0 +1,56 @@
+let currentIndex = 0;
+let personsData = [];
+
+fetchXML();
+let xhr = new XMLHttpRequest();
+function fetchXML()
+{
+    xhr.open("GET" , "data.xml" , true);
+    xhr.onreadystatechange = read ;
+    xhr.send(null);
+}
+
+function read() {
+    if (xhr.status === 200 && xhr.readyState === 4) {
+        let xmldoc = xhr.responseXML;
+        let persons = xmldoc.getElementsByTagName("person");
+
+        for (let i = 0; i < persons.length; i++) {
+            const person = {
+                name: persons[i].getElementsByTagName("name")[0].textContent.trim(),
+                age: persons[i].getElementsByTagName("age")[0].textContent.trim(),
+                city: persons[i].getElementsByTagName("city")[0].textContent.trim()
+            }
+
+            personsData.push(person);
+        }
+    }
+}
+
+function displayData(index)
+{
+    if(index>=0 && index < personsData.length)
+    {
+        document.getElementById("name").textContent = personsData[index].name;
+        document.getElementById("age").textContent = personsData[index].age;
+        document.getElementById("city").textContent = personsData[index].city;
+    }
+}
+
+function previousPerson()
+{
+    if(currentIndex > 0 )
+    {
+        currentIndex-- ;
+        displayData(currentIndex);
+    }
+}
+
+function nextPerson()
+{
+    if(currentIndex < personsData.length - 1)
+    {
+        currentIndex++;
+        displayData(currentIndex);
+    }
+}
